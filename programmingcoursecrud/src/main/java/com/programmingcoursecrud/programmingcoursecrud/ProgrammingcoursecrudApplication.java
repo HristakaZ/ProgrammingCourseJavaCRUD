0906c2ce3164b93843971119fa035f5e082e3e28
HristakaZ <hristaka2013@abv.bg>
2022-11-12 15:31:52 +0200

    Added tomcat server configuration, a sample controller, and a sample view

---
+++ programmingcoursecrud/src/main/java/com/programmingcoursecrud/programmingcoursecrud/ProgrammingcoursecrudApplication.java
@@ -0,0 +1,14 @@
+package com.programmingcoursecrud.programmingcoursecrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ProgrammingcoursecrudApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammingcoursecrudApplication.class, args);
+	}
+
+}
---
+++ programmingcoursecrud/src/test/java/com/programmingcoursecrud/programmingcoursecrud/ProgrammingcoursecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingcoursecrud.programmingcoursecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgrammingcoursecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
