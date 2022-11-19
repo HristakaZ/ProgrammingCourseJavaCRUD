74df4829a20faf5dd2b6cf8ba3ab4796b153021f
HristakaZ <hristaka2013@abv.bg>
2022-11-19 16:21:26 +0200

    Added connection to the database, installed NHibernate ORM, added needed dependencies for the CRUD operations

--- programmingcoursecrud/src/main/java/com/programmingcoursecrud/programmingcoursecrud/controller/GreetingController.java
+++ programmingcoursecrud/src/main/java/com/programmingcoursecrud/programmingcoursecrud/controller/GreetingController.java
@@ -3,12 +3,14 @@ package com.programmingcoursecrud.programmingcoursecrud.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class GreetingController {
-
     @GetMapping("/")
-    public String greeting(String message) {
-        message = "Hello, Ico";
+    public String greeting(Map<String, Object> model) {
+        model.put("message", "Hello Bryan");
         return "greeting";
     }
 }
